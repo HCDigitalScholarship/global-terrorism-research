dfc0474c89ba4a236fc5d330a91d7a0ee5b18a3c
apjanco <apjanco@gmail.com>
2017-07-07 13:09:57 -0400

    Update aqsistatement_migrate.sql

--- aqsistatement_migrate.sql
+++ aqsistatement_migrate.sql
@@ -3,26 +3,20 @@ from gtrp.node
 where type = 'aqsistatement' AND status = 1
 
 
---author
-field_data_field_author   field_author_value
-
 --title
 field_data_field_statement_title    field_statement_title_value
 
---access 
-field_data_field_access   field_access_value
-
---full-text
-field_data_field_full_text		field_full_text_title
+--statement id (ex. AAM20101023)
+field_data_field_statement_id   field_statement_id_value
 
 --issue date
 field_data_field_issue_date		field_issue_date_value
 
---organization
-field_data_field_jihad_organization		field_jihad_organization_value
+-- statement date
+field_data_field_statement_date   field_statement_date_value
 
---media type
-field_data_field_media_type		field_media_type_value
+--author
+field_data_field_author   field_author_value
 
 --produced by
 field_data_field_produced_by		field_produced_by_value
@@ -30,11 +24,18 @@ field_data_field_produced_by		field_produced_by_value
 --released by
 field_data_field_released_by		field_released_by_value
 
--- statement date
-field_data_field_statement_date   field_statement_date_value
+--media type
+field_data_field_media_type		field_media_type_value
 
---statement id (ex. AAM20101023)
-field_data_field_statement_id   field_statement_id_value
+--access 
+field_data_field_access   field_access_value
+
+--full-text
+field_data_field_full_text		field_full_text_title
+
+--organization
+field_data_field_jihad_organization		field_jihad_organization_value
+
 
 --keywords ??
 taxonomy_term_data  name tid vid
