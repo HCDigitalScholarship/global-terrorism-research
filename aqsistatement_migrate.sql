36af34b9b17cd6f8c3917a03e0bd04849bfaaa81
apjanco <apjanco@gmail.com>
2017-07-07 12:43:49 -0400

    Create aqsistatement_migrate.sql

---
+++ aqsistatement_migrate.sql
@@ -0,0 +1,29 @@
+select type, title
+from gtrp.node 
+where type = 'aqsistatement' AND status = 1
+
+
+--author
+field_data_field_author   field_author_value
+
+
+--access 
+field_data_field_access   field_access_value
+
+--full-text
+field_data_field_full_text		field_full_text_title
+
+--issue date
+field_data_field_issue_date		field_issue_date_value
+
+--organization
+field_data_field_jihad_organization		field_jihad_organization_value
+
+--media type
+field_data_field_media_type		field_media_type_value
+
+--produced by
+field_data_field_produced_by		field_produced_by_value
+
+--released by
+field_data_field_released_by		field_released_by_value
