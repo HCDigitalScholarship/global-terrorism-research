8e32a9118d3a56ed9870d1d9218b99f27f328a77
bbiney1 <bbiney1@swarthmore.edu>
2017-07-07 15:25:02 -0400

    Update aqsistatement_migrate.sql

--- aqsistatement_migrate.sql
+++ aqsistatement_migrate.sql
@@ -1,7 +1,19 @@
-select type, title, field_statement_title_value as "statement title"
+select type, title, field_statement_title_value as "statement title", `field_statement_id_value` as "statement id",`field_statement_id_value`as "statement date", `field_issue_date_value` as "issue date", `field_author_value` as "author", `field_released_by_value` as "released by",`field_produced_by_value` as "produced by", `field_media_type_value` as "media type", `field_full_text_url` as "full text link", `field_access_value` as "access"/*,`field_jihad_organization_value` as "organization"*/
 from gtrp.node n
-join gtrp.field_data_field_statement_title t on t.entity_id = n.nid  
+join gtrp.field_data_field_statement_title t on t.entity_id = n.nid
+join gtrp.field_data_field_statement_id e on e.entity_id = n.nid 
+join gtrp.field_data_field_statement_date d on d.entity_id = n.nid
+join gtrp.field_data_field_issue_date issued on issued.entity_id = n.nid
+join gtrp.field_data_field_author auth on auth.entity_id = n.nid
+join gtrp.field_data_field_produced_by prod on prod.entity_id = n.nid
+join gtrp.field_data_field_released_by relea on relea.entity_id = n.nid
+join gtrp.field_data_field_media_type med on med.entity_id = n.nid
+join gtrp.field_data_field_full_text urls on urls.entity_id = n.nid
+join gtrp.field_data_field_access doa on doa.entity_id = n.nid
+/*join gtrp.field_data_field_jihad_organization org on org.entity_id = n.nid*/
 where status = 1
+
+node id, run it through taxonomy index, gives you a tid, for a partiucular thing it can give 22 tids. If you run that through term_data then you get a name, but some entries have a parent. the taxonomy hiearchy gives the
 
 
 --title
