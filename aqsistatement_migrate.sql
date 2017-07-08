ca2277d47ad93de2527c9b30ab870384d9c05b95
apjanco <apjanco@gmail.com>
2017-07-07 22:34:12 -0400

    Update aqsistatement_migrate.sql

--- aqsistatement_migrate.sql
+++ aqsistatement_migrate.sql
@@ -1,4 +1,5 @@
-select type, title, field_statement_title_value as "statement title", `field_statement_id_value` as "statement id",`field_statement_id_value`as "statement date", `field_issue_date_value` as "issue date", `field_author_value` as "author", `field_released_by_value` as "released by",`field_produced_by_value` as "produced by", `field_media_type_value` as "media type", `field_full_text_url` as "full text link", `field_access_value` as "access"/*,`field_jihad_organization_value` as "organization"*/
+select type, field_statement_id_value as 'statement_id', title, field_statement_title_value as "statement title", field_statement_date_value as 'statement_date', field_issue_date_value as 'issue_date', field_author_value as 'author',  field_produced_by_value as 'produced_by', field_released_by_value as 'released_by', uid as 'posted_by', field_media_type_value as 'media_type', field_access_value as 'access', field_full_text_url as 'full_text_url', th.`parent` as 'context', td.`name` as 'keyword'
+
 from gtrp.node n
 join gtrp.field_data_field_statement_title t on t.entity_id = n.nid
 join gtrp.field_data_field_statement_id e on e.entity_id = n.nid 
@@ -9,9 +10,12 @@ join gtrp.field_data_field_produced_by prod on prod.entity_id = n.nid
 join gtrp.field_data_field_released_by relea on relea.entity_id = n.nid
 join gtrp.field_data_field_media_type med on med.entity_id = n.nid
 join gtrp.field_data_field_full_text urls on urls.entity_id = n.nid
-join gtrp.field_data_field_access doa on doa.entity_id = n.nid
-/*join gtrp.field_data_field_jihad_organization org on org.entity_id = n.nid*/
-where status = 1
+join gtrp.field_data_field_access doa on doa.entity_id = n.nid  
+join gtrp.taxonomy_index ti on n.nid = ti.nid
+join gtrp.taxonomy_term_data td on ti.tid = td.tid
+join gtrp.taxonomy_term_hierarchy th on ti.tid = th.tid  
+where status = 1;
+
 
 node id, run it through taxonomy index, gives you a tid, for a partiucular thing it can give 22 tids. If you run that through term_data then you get a name, but some entries have a parent. the taxonomy hiearchy gives the
 
