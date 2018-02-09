eec9639aa6f8d3c01f2908c3f1ad822113d37f9b
Ian Fisher <ianfisher45@gmail.com>
2018-02-09 20:55:09 +0000

    Simplify search_results.js and remove old code

--- gtr_site/static/gtr_site/js/search_results.js
+++ gtr_site/static/gtr_site/js/search_results.js
@@ -1,14 +1,11 @@
-var checkboxes = {};
+// Global variables that hold the names of keywords to be included and excluded.
+var include = [];
+var exclude = [];
 
 $(document).ready(function () {
-    for (var i = 0; i < allKeywords.length; i++) {
-        checkboxes[allKeywords[i]] = {
-            name: allKeywords[i], count: 0, include: false, exclude: false
-        };
-    }
-
     var table = $("#mainTable").DataTable({
-        columnDefs: [
+        "pageLength": 50,
+        "columnDefs": [
           {visible: false, targets: [2]}
         ]
     });
@@ -16,31 +13,22 @@ $(document).ready(function () {
 
     // Inspired by stackoverflow.com/questions/30086341
     $.fn.dataTable.ext.search.push(function (settings, data, dataIndex) {
-        for (var key in checkboxes) {
-            if (checkboxes.hasOwnProperty(key) && checkboxes[key].include) {
-                if (!data[2].includes('|' + key + '|')) {
-                    return false;
-                }
+        for (var i = 0; i < include.length; i++) {
+            if (!data[2].includes('|' + include[i] + '|')) {
+                return false;
             }
         }
-        for (var key in checkboxes) {
-            if (checkboxes.hasOwnProperty(key) && checkboxes[key].exclude) {
-                if (data[2].includes('|' + key + '|')) {
-                    return false;
-                }
+        for (var i = 0; i < exclude.length; i++) {
+            if (data[2].includes('|' + exclude[i] + '|')) {
+                return false;
             }
         }
         return true;
     });
 
-    $(".include-checkbox").change(makeCallback(table, "include"));
-    $(".exclude-checkbox").change(makeCallback(table, "exclude"));
+    $(".include-checkbox").change(makeCallback(table, include));
+    $(".exclude-checkbox").change(makeCallback(table, exclude));
 });
-
-
-function parseKeywordStr(keywordStr) {
-    return keywordStr.substring(1, keywordStr.length - 1).split('""');
-}
 
 
 function renderKeywords(table) {
@@ -108,114 +96,16 @@ function checkboxCount(elem) {
 }
 
 
-// Render the keywords and their counts in the "Include" and "Exclude" columns.
-//   This function takes the DataTable object as a parameter, because that is where all the data is
-//   ultimately stored.
-//
-//   Note: This function uses (but does not modify) the global variable `checkboxes`.
-function renderKeywords_OLD(table) {
-    for (var key in checkboxes) {
-        if (checkboxes.hasOwnProperty(key)) {
-            checkboxes[key].count = 0;
-        }
-    }
-
-    // Calculate the count of each keyword.
-    table.rows({search: 'applied'}).every(function (rowIdx, tableLoop, rowLoop) {
-        var data = this.data();
-        var keywords = data[2].split('|');
-        for (var i = 0; i < keywords.length; i++) {
-            var keyword = keywords[i];
-            if (keyword.length !== 0) {
-                if (keyword in checkboxes) {
-                    checkboxes[keyword].count++;
-                } else {
-                    console.log(keyword);
-                }
-            }
-        }
-    });
-
-    var includeKeywords = getIncludeKeywords();
-    var excludeKeywords = getExcludeKeywords();
-
-    $("#include-buttons").empty();
-    $("#exclude-buttons").empty();
-
-    for (var i = 0; i < includeKeywords.length; i++) {
-        var item = includeKeywords[i];
-        $("#include-buttons").append(makeCheckbox(item, "include"));
-    }
-    for (var i = 0; i < excludeKeywords.length; i++) {
-        var item = excludeKeywords[i];
-        $("#exclude-buttons").append(makeCheckbox(item, "exclude"));
-    }
-}
-
-
-function makeCallback(table, propertyToUpdate) {
+function makeCallback(table, arrayToUpdate) {
     return function () {
         var name = $(this).attr("name");
         if (this.checked) {
-            checkboxes[name][propertyToUpdate] = true;
+            arrayToUpdate.push(name);
         } else {
-            checkboxes[name][propertyToUpdate] = false;
+            var i = arrayToUpdate.indexOf(name);
+            arrayToUpdate.splice(i, 1);
         }
         table.draw();
         renderKeywords(table);
     };
-}
-
-
-// Return an HTML string for a keyword checkbox, where `type` is either 'exclude' or 'include'.
-function makeCheckbox(keywordItem, property) {
-      return "<li class=\"list-group-item justify-content-between\">" +
-        "<input type=\"checkbox\" name=\"" + keywordItem.name + "\" value=\"key_OFF\"" +
-               "class=\"filter_check form-check-input " + property + "-checkbox\"" +
-               (keywordItem[property] ? " checked" : "") + ">" +
-          "   " + keywordItem.name + "<span class=\"badge badge-default badge-pill\">" + keywordItem.count + "</span>" +
-      "</li>";
-}
-
-
-function getIncludeKeywords() {
-    var keywordsCopy = Object.values(checkboxes);
-    keywordsCopy.sort(makeCompareFunction('include'));
-    return keywordsCopy.slice(0, 20);
-}
-
-
-function getExcludeKeywords() {
-    var keywordsCopy = Object.values(checkboxes);
-    keywordsCopy.sort(makeCompareFunction('exclude'));
-    return keywordsCopy.slice(0, 20);
-}
-
-
-// Make a function that compare two checkbox objects on the given property, either 'include' or
-//   'exclude'.
-function makeCompareFunction(prop) {
-    // Compare two checkbox objects. Sort checked boxes first, then sort by count, then by name.
-    function cmp(a, b) {
-        if (a[prop] && !b[prop]) {
-            return -1;
-        } else if (!a[prop] && b[prop]) {
-            return 1;
-        } else {
-            if (a.count > b.count) {
-                return -1;
-            } else if (a.count < b.count) {
-                return 1;
-            } else {
-                if (a.name < b.name) {
-                    return -1;
-                } else if (a.name > b.name) {
-                    return 1;
-                } else {
-                    return 1;
-                }
-            }
-        }
-    }
-    return cmp;
 }
