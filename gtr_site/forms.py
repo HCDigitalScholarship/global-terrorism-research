2ebd8fbacd117fd5ca7aae584d1a32af0db2be96
Alex Gleason <alex@alexgleason.me>
2023-05-13 11:22:43 -0500

    forms.py: move boot-time code to runtime so migrations can happen without a database

--- gtr_site/forms.py
+++ gtr_site/forms.py
@@ -22,15 +22,15 @@ from django.contrib.admin.widgets import FilteredSelectMultiple, RelatedFieldWid
 #A user needs to be able to select all Keywords that they want and then have a filter_horizontal field come up for that.
 
 ######################################
-clipboards = List.objects.all()
-CHOICES = []
-count = 0
-for item in clipboards:
-  count += 1
-  CHOICES.append((str(count),item.list_name))
-
 class ListForm(forms.Form):
-  list_name = forms.ChoiceField(choices=CHOICES)
+    clipboards = List.objects.all()
+    CHOICES = []
+    count = 0
+    for item in clipboards:
+        count += 1
+        CHOICES.append((str(count),item.list_name))
+
+    list_name = forms.ChoiceField(choices=CHOICES)
 ######################################
 
 class KeywordInContextForm(forms.ModelForm):
