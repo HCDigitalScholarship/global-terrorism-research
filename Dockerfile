75bd447383b233b4851fdf8e29d881c91aded448
Alex Gleason <alex@alexgleason.me>
2023-05-13 11:36:59 -0500

    Add whitenoise for static files

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,8 @@ RUN pip install -r /app/requirements.txt
 
 COPY . /app
 
+RUN SECRET_KEY=1 python manage.py collectstatic --noinput --clear
+
 EXPOSE 8000
 
 CMD python manage.py migrate; python manage.py runserver 0.0.0.0:8000
--- gtr/settings.py
+++ gtr/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,7 +138,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = '/srv/global-terrorism-research/gtr_site/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'gtr_site', 'static')
 
 
 LOGGING = {
