b292f9c1643f538b7a787e028887a7698a0ec86a
Alex Gleason <alex@alexgleason.me>
2023-05-13 12:20:02 -0500

    Move sqlite database to a subdirectory so it can be easily mounted as a docker volume

--- gtr/settings_docker.py
+++ gtr/settings_docker.py
@@ -13,10 +13,3 @@ DEBUG = os.environ.get('DEBUG', '').lower() in ['true', '1']
 ALLOWED_HOSTS = ['*']
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
