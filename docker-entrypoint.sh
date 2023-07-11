b40293c1edbd90e93fb5ba8cd7a3ec3a01dd42cb
Alex Gleason <alex@alexgleason.me>
2023-07-11 12:54:48 -0500

    Remove unnecessary migrations

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,10 +2,11 @@
 set -e
 
 echo "-- Waiting for database..."
+# HACK: not sure how to wait for mysql to be ready
 sleep 3
 
-# echo "-- Running migrations..."
-# python manage.py migrate --noinput
+echo "-- Running migrations..."
+python manage.py migrate --noinput
 
 echo "-- Starting"
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:${PORT:-8000}
--- gtr_site/migrations/0034_remove_list_user.py
+++ gtr_site/migrations/0034_remove_list_user.py
@@ -12,8 +12,8 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        #migrations.RemoveField(
-        #    model_name='list',
-        #    name='user',
-        #),
+        migrations.RemoveField(
+           model_name='list',
+           name='user',
+        ),
     ]
